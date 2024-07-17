7de814f4821ae341064b04801130d997748ce1d9
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-17 16:08:16 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+public static void main(String[]args){
+// Create Object for Person
+  // Initialize values with Constructors
+  // print the values
+  // Create another Object for Person
+  // Assign Values with Getters & Setters
+  // print the values
+}
+}
