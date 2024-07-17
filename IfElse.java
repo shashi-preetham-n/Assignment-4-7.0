2491f06c49f58e116d8d51a1b55a3543a4953e6c
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-17 17:04:02 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+// prepare Grade sheet by using IF ELSE use following details
+  // Score greater than 90 and less than or equal to 100 A+
+// Score greater than 80 and less than or equal 90 A
+// Score greater than 70 and less than or equal 80 B+
+// Score Greater than 60 and less than or equal 50 B
+// Like Wise the person who gets less than 36 is considered as Grade F
+// Variables should use
+public class IfElse{
+  public int score=60;
+  public static void main(String[]args){
+IfElse f=new IfElse();
+   // Complete the Reamaining Logic using score variable for a value
+}
