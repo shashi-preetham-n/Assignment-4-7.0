ca964c59c8330fe3bab224b7b77e9684be02910a
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-17 16:16:47 +0530

    Create BugFix.java

---
+++ BugFix.java
@@ -0,0 +1,32 @@
+public class BugFix{
+ private int Bid;
+  private String Bname;
+  
+  public int getBid() {
+    return Bid;
+}
+
+public void setBid(int bid) {
+    Bid = bid;
+}
+
+public String getBname() {
+    return Bname;
+}
+
+public void setBname(String bname) {
+    Bname = bname;
+}
+
+public BugFix(int bid, String bname) {
+    this.Bid = bid;
+    this.Bname = bname;
+}
+
+public static void main(String[]args){
+ BugFix b=new BugFix();
+  b.setBid(1);
+  b.setBname("");
+  System.out.println(b.getBid());
+  System.out.println(b.getBname());
+  }}
